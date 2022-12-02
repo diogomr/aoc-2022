55409af371324566fd34c75f16a2313c0535ab99
diogomr <diogo.moreira@amplemarket.com>
2022-12-02 08:57:19 +0000

    Day02 but not ugly

--- day02/part1.rb
+++ day02/part1.rb
@@ -2,50 +2,32 @@
 
 lines = File.read("input.txt").split("\n")
 
-MOVE_SCORE = {
-  "X" => 1,
-  "Y" => 2,
-  "Z" => 3
-}
 WIN_SCORE = 6
 DRAW_SCORE = 3
+
+MOVE_SCORING = {
+  A: {
+    X: 1 + DRAW_SCORE,
+    Y: 2 + WIN_SCORE,
+    Z: 3
+  },
+  B: {
+    X: 1,
+    Y: 2 + DRAW_SCORE,
+    Z: 3 + WIN_SCORE
+  },
+  C: {
+    X: 1 + WIN_SCORE,
+    Y: 2,
+    Z: 3 + DRAW_SCORE
+  }
+}
+
 
 res = lines.sum do |line|
   (opp, me) = line.split(" ")
 
-  if opp == "A"
-    if me == "X"
-      MOVE_SCORE[me] + DRAW_SCORE
-    elsif me == "Y"
-      MOVE_SCORE[me] + WIN_SCORE
-    elsif me == "Z"
-      MOVE_SCORE[me]
-    else
-      raise "Error: #{opp}, #{me}"
-    end
-  elsif opp == "B"
-    if me == "X"
-      MOVE_SCORE[me]
-    elsif me == "Y"
-      MOVE_SCORE[me] + DRAW_SCORE
-    elsif me == "Z"
-      MOVE_SCORE[me] + WIN_SCORE
-    else
-      raise "Error: #{opp}, #{me}"
-    end
-  elsif opp == "C"
-    if me == "X"
-      MOVE_SCORE[me] + WIN_SCORE
-    elsif me == "Y"
-      MOVE_SCORE[me]
-    elsif me == "Z"
-      MOVE_SCORE[me] + DRAW_SCORE
-    else
-      raise "Error: #{opp}, #{me}"
-    end
-  else
-    raise "Error: #{opp}, #{me}"
-  end
+  MOVE_SCORING[opp.to_sym][me.to_sym]
 end
 
 p res
