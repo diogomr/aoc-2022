a98eeac66fd20182cdb39fb54d64939e60e556bc
diogomr <diogo.moreira@amplemarket.com>
2022-12-06 08:51:13 +0000

    Day06

---
+++ day06/part1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+(lines,_) = File.read("input.txt").split("\n")
+
+BLOCK_SIZE = 4
+
+count = 0
+lines.split("").each_cons(BLOCK_SIZE) do |block|
+  if block == block.uniq
+    break
+  end
+  count+=1
+end
+
+p count + BLOCK_SIZE
